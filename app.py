4216eb4041f06f7496f4ad4d80f5dbb255040777
Ubuntu <azureuser@myVM.twk42ystwwrelfmvbyr4q2gkjc.tx.internal.cloudapp.net>
2024-07-16 11:29:05 +0000

    Add deploy workflow and update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Kanduri Venkata Sesha Ramanujam: Hello World!'
+def hello():
+    return 'Hello, ramanujamkanduri!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
